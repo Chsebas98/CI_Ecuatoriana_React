08cc9651a615246b36f0cfe499162050a79a4158
Chsebas98 <chsebas98@gmail.com>
2022-01-22 21:18:56 -0500

    Aumento de ruta / para la app

--- src/routes/LoginRouter.jsx
+++ src/routes/LoginRouter.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "../Pages/Register";
 
 const LoginRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route end path="/register" element={<Register />}></Route>
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route end path="/" element={<Register />}></Route>
+			</Routes>
+			<Routes>
+				<Route end path="/register" element={<Register />}></Route>
+			</Routes>
+		</Router>
+	);
 };
 
 export default LoginRouter;
